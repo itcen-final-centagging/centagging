bab2666a4421d336748e3c16241d0c1731fe1097
영관 <ykstar78@gmail.com>
2026-08-10 11:39:42 +0900

    fix: crop 시 bbox 좌표 변경(SCRUM-118)

--- app/services/image_processing_service.py
+++ app/services/image_processing_service.py
@@ -3,7 +3,7 @@ from PIL import Image
 def get_crop_image(image: Image.Image, bbox) -> Image.Image:
     left = round(bbox["xmin"] / 1000 * image.width)
     right = round(bbox["xmax"] / 1000 * image.width)
-    upper = round(bbox["ymax"] / 1000 * image.height)
-    lower = round(bbox["ymin"] / 1000 * image.height)
+    upper = round(bbox["ymin"] / 1000 * image.height)
+    lower = round(bbox["ymax"] / 1000 * image.height)
 
     return image.crop((left, upper, right, lower))
